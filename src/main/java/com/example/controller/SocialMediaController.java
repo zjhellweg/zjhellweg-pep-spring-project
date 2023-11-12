b0683ee45ae25056fa3cc50d05c9a6aa7653012d
Zachary Hellweg <Zjhellweg@gmail.com>
2023-11-12 06:39:12 +0000

    Hellweg Progress Update

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,24 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.hibernate.mapping.Array;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Message;
+import com.example.exception.InvalidMessageLength;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +26,45 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    private MessageService messageService;
+
+    public SocialMediaController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getAllMessages(){
+        List<Message> returnValue = messageService.getAllMessages();
+        return returnValue;
+    }
+    
+    @GetMapping("/messages/{id}")
+    public Message getMessageById(@PathVariable int id) throws Exception{
+        Message returnValue = messageService.getMessageById(id);
+        return returnValue;
+    }
+    
+    @DeleteMapping("messages/{id}")
+    public Integer deleteMessageById(@PathVariable int id) throws Exception{
+        int before = messageService.getAllMessages().size();
+        messageService.deleteMessageById(id);
+        int after = messageService.getAllMessages().size();
+        if(before - after != 0){
+            return before-after;
+        }else {
+            return null;
+        }
+    }
+    
+    @PostMapping("messages")
+    public ResponseEntity<Message> insertMessage(@RequestBody Message message) throws InvalidMessageLength{
+        Message returnMessage = messageService.addMessage(message);
+        if(returnMessage != null){
+            return new ResponseEntity(returnMessage,HttpStatus.valueOf(200));
+        }
+        return new ResponseEntity(null,HttpStatus.valueOf(400));
+    }
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,56 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+import com.example.entity.Message;
+import com.example.exception.InvalidMessageLength;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
+    MessageRepository repository;
+
+    @Autowired
+    public MessageService(MessageRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Message> getAllMessages(){
+        return repository.findAll();
+    }
+
+    public Message getMessageById(int id){
+            Optional<Message> returnValue = repository.findById(id);
+            if (returnValue.isPresent()) {
+                return returnValue.get();
+            } else {
+                return null;
+            }
+    }
+
+    public Message deleteMessageById(int id){
+        Optional<Message> returnValue = repository.findById(id);
+        if(returnValue.isPresent()){
+            repository.deleteById(id);
+            return new Message(); //returnValue.get();
+        }
+        return null;
+
+    }
+
+    public Message addMessage(Message message) throws InvalidMessageLength {
+        Message returnMessage = new Message();
+        if(message.getMessage_text().length() > 254 || message.getMessage_text().equals("")){
+            return null;
+        }
+        returnMessage = repository.save(message);
+
+        return returnMessage;
+    }
 }
